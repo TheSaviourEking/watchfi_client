93c981590befe144effd571f04751d786e246c41
TheSaviourEking <saviour.eking@cyberservices.com>
2025-07-21 16:20:01 +0100

    add config files

---
+++ src/config/apiConfig.js
@@ -0,0 +1,23 @@
+const getApiBaseUrl = () => {
+    return import.meta.env.PROD
+        ? import.meta.env.VITE_API_BASE_URL_PRODUCTION || 'https://watchfi-prod.onrender.com/'
+        : import.meta.env.VITE_API_BASE_URL_DEVELOPMENT || 'http://localhost:5000/';
+};
+
+const api = {
+    baseURL: getApiBaseUrl(),
+    endpoints: {
+        collections: '/api/v1/collections',
+        collectionDetails: '/api/v1/collections/',
+        filter: '/api/v1/filter',
+        customers: '/api/v1/customers',
+        bookings: '/api/vi/bookings',
+        verifyBookings: '/api/v1/bookings/verify'
+    },
+    getUrl: function (endpoint, param = '') {
+        // console.log(`${this.baseURL}${this.endpoints[endpoint]}${param}`);
+        return `${this.baseURL}${this.endpoints[endpoint]}${param}`;
+    }
+};
+
+export default api;
